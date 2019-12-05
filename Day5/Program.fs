83d942afbc8a1b85c83de1cf75ec26ae760eda8c
Emil <emil.astrom@consid.se>
2019-12-05 16:09:47 +0100

    init

--- Day5/Program.fs
+++ Day5/Program.fs
@@ -1,8 +1,8 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
+open Day5.IntComputer
 
 [<EntryPoint>]
 let main argv =
+    run ""
     printfn "Hello World from F#!"
     0 // return an integer exit code
